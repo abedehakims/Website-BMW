b1dbd5e5d5c589a88f599f19ce3b69d3af83e1d8
mister abdul <ahakimmaulana07@gmail.com>
2025-12-05 17:54:41 +0700

    Add files via upload

---
+++ Website BMW/js folder/main.js
@@ -0,0 +1,100 @@
+/*-------BAGIAN SHOW MENU--------*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+/*Menampilkan menu*/
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*Menyembunyikan menu*/
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*--------Menghilangkan menu di versi Mobile----*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*--------SWIPER HOME----*/
+const swiperHome = new Swiper('.home__swiper', {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: (index, className) => {
+            return '<span class="' + className + '">' + String(index + 1).padStart(2, '0') + "</span>";
+        },
+    },
+
+    autoplay: {
+        delay: 6000,
+    }
+})
+
+/* ------------ Mengganti Background Header ---------- */
+const bgHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+        : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', bgHeader)
+
+/* -------- Menampilkan SCROLL UP --------- */
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/* -------- AKTIVASI SCROLL UNTUK LINK PER SECTION --------- */
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/* -------- ANIMASI SCROLL REVEAL --------- */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 300,
+    // reset = true, // animasi diulang
+})
+
+sr.reveal(`.home__bg`, { scale: 1.1, opacity: 1 })
+sr.reveal(`.home__swiper`, { origin: 'right', distance: '300px', delay: 800 })
+sr.reveal(`.home__data`, { origin: 'bottom', distance: '120px', delay: 1600 })
+sr.reveal(`.swiper-pagination-bullet`, { origin: 'top', delay: '1800', opacity: 0 })
+sr.reveal(`.home__button`, { origin: 'top', delay: 2200 })
+sr.reveal(`.about__data, .contact__content`, { origin: 'left' })
+sr.reveal(`.about__video, .contact__img`, { origin: 'right' })
+sr.reveal(`.models__card`, { interval: 100 })
+sr.reveal(`.info__img`, { distance: '120px' })
+sr.reveal(`.info__number`, { origin: 'bottom', distance: '80px', delay: 800 })
+sr.reveal(`.info__group`, {})
+sr.reveal(`.footer__container`)
